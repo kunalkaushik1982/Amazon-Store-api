2789fce69095cd7c94adcff993ca0c6964c01df2
kunalkaushik1982 <kunal.kaushik1982@gmail.com>
2022-03-02 00:29:24 +0530

    Added EndPoints

--- code/app.py
+++ code/app.py
@@ -1,9 +1,38 @@
+from crypt import methods
 from flask import Flask
 
 app = Flask(__name__) 
+
+
 
 @app.route('/')
 def Hello():
     return "Hello World..!!"
 
-app.run(port=5000,)
+#POST /store data: {name:}
+@app.route('/store',methods=['POST'])
+def create_store():
+    pass
+
+#GET /store/<string:name>
+@app.route('/store/<string:name>')
+def get_store(name):
+    pass
+
+#GET /store
+@app.route('/store')
+def get_stores():
+    pass
+
+#POST /store/<string:name>/item {item: {name:, price:}}
+@app.route('/store/<string:name>/item',methods=['POST'])
+def create_item_in_store(name):
+    pass
+
+#GET /store/<string:name>/item
+@app.route('/store/<string:name>/item')
+def get_item_in_store(name):
+    pass
+
+
+app.run(port=5000)
